bccccbf29d5f06bedea9481e118212eecd588286
johanazapata <johanazapata2012@gmail.com>
2020-04-07 12:04:32 -0400

    nuevo estatus

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import cipher from './cipher.js';
+function validar() {
+    let men_code = "";
+    let mensa = "";
+    let num = document.getElementById("desplazar").value;
+    let largo = document.getElementById("mensaje_cifrar").value;
+    let largo1 = document.getElementById("mensaje_cifrar").value.length;
 
-console.log(cipher);
+    for (a = 0; a <= largo1; a++) {
+        men_code = (largo.charCodeAt([a]) - 65 + (num)) % 26 + 65;
+        //document.write(men_code + "<br>");
+        mensa += String.fromCharCode(men_code)
+        //document.write(mensa + "<br>"); 
+    }
+    document.getElementById("mensaje_cifrado").value = mensa;
+
+}
+
+
+
