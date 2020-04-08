a84f53eab0692f80a443c463b5f8d89e98cfc73c
johanazapata <johanazapata2012@gmail.com>
2020-04-08 18:36:08 -0400

    correcion index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-function validar() {
+function cifraMen() {
     let men_code = "";
     let mensa = "";
     let num = document.getElementById("desplazar").value;
     let largo = document.getElementById("mensaje_cifrar").value;
     let largo1 = document.getElementById("mensaje_cifrar").value.length;
 
-    for (a = 0; a <= largo1; a++) {
-        men_code = (largo.charCodeAt([a]) - 65 + (num)) % 26 + 65;
-        //document.write(men_code + "<br>");
-        mensa += String.fromCharCode(men_code)
-        //document.write(mensa + "<br>"); 
-    }
+    let Cifrar = window.cipher.cifrar(cifraMen, num);
+
     document.getElementById("mensaje_cifrado").value = mensa;
 
+    Cifrar.addEventListener("click", cifraMen)
 }
 
 
+function deCifra() {
+    let men_code1 = "";
+    let mensa1 = "";
+    let num1 = document.getElementById("desplazar").value;
+    let largo2 = document.getElementById("mensaje_cifrar").value;
+    let largo3 = document.getElementById("mensaje_cifrar").value.length;
+
+    let Decifrar = window.cipher.decifrar(deCifra, num1);
+    Decifrar.addEventListener("click", deCifra)
+
+    document.getElementById("mensaje_cifrado").value = mensa1;
+}
 
